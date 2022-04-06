549cd11c9c565fc8ecffff8fe762b1beacde1dfc
07B05\07bstudent <hmj2122053@stu.o-hara.jp>
2022-04-06 09:52:24 +0900

    demo コントローラ振動完成

--- Assets/Script/Stage_Clear.cs
+++ Assets/Script/Stage_Clear.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class Stage_Clear : MonoBehaviour
@@ -27,6 +28,30 @@ public class Stage_Clear : MonoBehaviour
             ui.SetActive(true);
             FirstButton.Select();
 
+            StartVibration(1.0f, 0.7f, 0.7f);
+
         }
+    }
+
+    //コントローラー振動
+    public void Vibration(float l, float r)
+    {
+        Gamepad.current.SetMotorSpeeds(l, r);
+    }
+
+    public void StopVibration()
+    {
+        Gamepad.current.SetMotorSpeeds(0, 0);
+    }
+    private IEnumerator LoopVibration(float t, float l, float r)
+    {
+        Gamepad.current.SetMotorSpeeds(l, r);
+        yield return new WaitForSeconds(t);
+        StopVibration();
+    }
+
+    public void StartVibration(float t, float l, float r)
+    {
+        StartCoroutine(LoopVibration(t, l, r));
     }
 }
