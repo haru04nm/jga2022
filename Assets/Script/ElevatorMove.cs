4d48fef192918ed962cbcdb03eae33adbe75586b
松岡育弥 <hmj2122051@stu.o-hara.ac.jp>
2022-04-13 13:56:45 +0900

    エレベーター降りたら動かなくなりました

--- Assets/Script/ElevatorMove.cs
+++ Assets/Script/ElevatorMove.cs
@@ -57,6 +57,10 @@ public class ElevatorMove : MonoBehaviour
                     DownMove();
                 }
             }
+            else
+            {
+                isHitFlag = false;
+            }
         }
     }
 
