b1f9f33d65f88c8083b425c4ce3ef8f9bbda2695
松岡育弥 <hmj2122051@stu.o-hara.ac.jp>
2022-04-14 11:28:15 +0900

    スイッチ途中

--- Assets/Script/Switch_Setactive.cs
+++ Assets/Script/Switch_Setactive.cs
@@ -7,12 +7,11 @@ public class Switch_Setactive : MonoBehaviour
     [SerializeField]
     GameObject[] _switch; 
 
-    // Start is called before the first frame update
     void Start()
     {
+
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         for (int x=0;x<_switch.Length;x++)
