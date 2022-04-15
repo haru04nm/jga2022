41cf830777a0fdeab84334b7f1a091142fcb0aa8
亜香音 <hmj2122002@stu.o-hara.ac.jp>
2022-04-15 14:08:10 +0900

    Stage5 途中

--- Assets/Script/Domino.cs
+++ Assets/Script/Domino.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Domino : MonoBehaviour
 {
+    [SerializeField]
+    GameObject barrel;
+
+    /*
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,14 @@ public class Domino : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-}
+    */
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == barrel)
+        {
+
+        }
+    }
