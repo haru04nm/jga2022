88377e422648e5f525717dfa4f8fadb6f465e1fd
亜香音 <hmj2122002@stu.o-hara.ac.jp>
2022-04-20 10:47:41 +0900

    棘ギミック完成

--- Assets/Script/Switch_Door.cs
+++ Assets/Script/Switch_Door.cs
@@ -7,11 +7,18 @@ public class Switch_Door : MonoBehaviour
     static bool redFlag;
     static bool blueFlag;
 
-    [SerializeField]
-    GameObject door1;
+    static bool thornFlag;
 
     [SerializeField]
-    GameObject door2;
+    GameObject[] door;
+
+    [SerializeField]
+    int doorNum;
+
+    GameObject[] thorn;
+
+    [SerializeField]
+    int thornNum;
 
     bool switchActive;
 
@@ -21,12 +28,36 @@ public class Switch_Door : MonoBehaviour
         blueFlag = false;
         redFlag = true;
         switchActive = false;
-        door1.gameObject.SetActive(redFlag);
-        door2.gameObject.SetActive(blueFlag);
+        thornFlag = true;
 
+        if (doorNum!=0)
+        {
+            for (int i=0;i<doorNum;i++)
+            {
+                door[i].gameObject.SetActive(redFlag);
+
+                if (i%2==1)
+                {
+                    door[i].gameObject.SetActive(blueFlag);
+                }
+            }
+        }
+
+        if (thornNum!=0)
+        {
+            thorn = new GameObject[thornNum];
+
+            for (int i=0;i<thornNum;i++)
+            {
+                thorn[i] = GameObject.Find("Toge ("+(i+1)+")").gameObject;
+            }
+        
+            foreach(var i in thorn)
+            {
+                i.SetActive(thornFlag);
+            }
+        } 
     }
-
-
 
     private void OnTriggerEnter(Collider other)
     {
@@ -34,9 +65,25 @@ public class Switch_Door : MonoBehaviour
 
         redFlag = !redFlag;
         blueFlag = !blueFlag;
+        thornFlag = !thornFlag;
 
-        door1.gameObject.SetActive(redFlag);
-        door2.gameObject.SetActive(blueFlag);
+        if (doorNum != 0)
+        {
+            for (int i = 0; i < doorNum; i++)
+            {
+                door[i].gameObject.SetActive(redFlag);
+
+                if (i % 2 == 1)
+                {
+                    door[i].gameObject.SetActive(blueFlag);
+                }
+            }
+        }
+
+        foreach (var i in thorn)
+        {
+            i.SetActive(false);
+        }
     }
 
     public bool IsSwitchActive
