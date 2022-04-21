af8f0cf8b7bbd98e922b18db2e67c14d8a0dde69
松岡育弥 <hmj2122051@stu.o-hara.ac.jp>
2022-04-21 15:09:26 +0900

    ステージ１０途中

--- Assets/Script/Rotate.cs
+++ Assets/Script/Rotate.cs
@@ -6,7 +6,7 @@ public class Rotate : MonoBehaviour
 {
     private void Update()
     {
-        this.transform.Rotate(new Vector3(0, 0, -45) * Time.deltaTime);
+        this.transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
     }
 
 }
