f6e744f5dad834f47cbcae168039d9d9153ad68d
陸駆 <hmj2122053@stu.o-hara.jp>
2022-04-21 15:10:27 +0900

    spot 完成

--- Assets/Script/Barrel.cs
+++ Assets/Script/Barrel.cs
@@ -32,19 +32,25 @@ public class Barrel : MonoBehaviour
         count += Time.deltaTime;
 
         //spotLight　OFF
-        spotLight.SetActive(false);
+        //spotLight.SetActive(false);
 
         mainLight.transform.rotation = new Quaternion(30, -30,0,0);
 
-        if(oldPosX != transform.position.x && count >= 1.0f)
+        if(oldPosX != transform.position.x)
         {
-            //コントローラー振動
-            StartVibration(0.1f, 0.7f, 1f);
-            count=0.0f; 
+
+            if (count >= 1.0f)
+            {
+                //コントローラー振動
+                StartVibration(0.1f, 0.7f, 1f);
+                count=0.0f; 
+            }
+            
             rb.velocity = new Vector3(Mathf.Clamp((rb.velocity.x * speed),-speed,speed),rb.velocity.y, rb.velocity.z);
 
             //spotLight　ON
             spotLight.SetActive(true);
+            spotLight.transform.position = new Vector3(transform.position.x, spotLight.transform.position.y, spotLight.transform.position.z);
 
             mainLight.transform.rotation = new Quaternion(180, 30, 0, 0);
         }
