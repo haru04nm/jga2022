66c180e167515520f241bc9c3a091d330fac7f55
亜香音 <hmj2122002@stu.o-hara.ac.jp>
2022-04-25 11:57:27 +0900

    StageSelect プレイヤームーブ完成

--- Assets/Script/Domino.cs
+++ Assets/Script/Domino.cs
@@ -6,28 +6,4 @@ public class Domino : MonoBehaviour
 {
     [SerializeField]
     GameObject barrel;
-
-    /*
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    */
-
-    /*
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject == barrel)
-        {
-
-        }
-    }
-    */
 }
--- Assets/Script/説明書用/PlayerMove.cs
+++ Assets/Script/説明書用/PlayerMove.cs
@@ -6,9 +6,12 @@ public class PlayerMove : MonoBehaviour
 {
     Rigidbody rbody;
     private Animator animator;
-    private Vector2 _moveInoutValue;
     GameObject body;
-    RaycastHit hit;
+
+    int move;
+    const int moveNum = 2;
+
+    bool turnFlag;
 
     // Start is called before the first frame update
     void Start()
@@ -16,17 +19,36 @@ public class PlayerMove : MonoBehaviour
         rbody = GetComponent<Rigidbody>();
         animator = transform.Find("Mesh Object").gameObject.GetComponent<Animator>();
         body = GameObject.Find("Body").gameObject;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        turnFlag = false;
     }
 
     private void FixedUpdate()
     {
-        rbody.velocity = new Vector3(2,0,0);
+        if (this.transform.position.x >= -14f && this.transform.position.x <= 14f)
+        {
+            move = turnFlag ? -moveNum : moveNum;
 
-        animator.SetFloat("Move", rbody.velocity.magnitude);
+            transform.rotation = Quaternion.Euler(0, turnFlag ? -90 : 90, 0);
+
+            rbody.velocity = new Vector3(move, 0, 0);
+
+            animator.SetFloat("Move", rbody.velocity.magnitude);
+
+            if (!turnFlag)
+            {
+                if (this.transform.position.x >= 13f)
+                {
+                    turnFlag = true;
+                }
+            }
+            else 
+            {
+                if (this.transform.position.x <= -13f)
+                {
+                    turnFlag = false;
+                }
+            }
+        }
     }
 }
