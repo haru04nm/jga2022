97f2b1e6e6d0f022c20199625a36765bd317211c
陸駆 <hmj2122053@stu.o-hara.jp>
2022-04-27 10:50:26 +0900

    暗い演出 OK

--- Assets/Script/Barrel.cs
+++ Assets/Script/Barrel.cs
@@ -5,79 +5,42 @@ using UnityEngine.InputSystem;
 
 public class Barrel : MonoBehaviour
 {
-    float oldPosX;
-
-    float count;
-
-    float posY;
-
-    [SerializeField]
     GameObject spotLight;
 
-    [SerializeField]
-    Transform mainLight;
-    
-    float speed=3.5f;
+    float oldPosX;
 
-    Rigidbody rb;
+    bool lightFlag=false; 
+
+    private void Start()
+    {
+        spotLight = this.transform.GetChild(1).gameObject;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     { 
-        rb= this.gameObject.GetComponent<Rigidbody>();
-        count += Time.deltaTime;
-
-       
-
         //spotLight　OFF
-        //spotLight.SetActive(false);
+        spotLight.SetActive(false);
 
-        mainLight.transform.rotation = new Quaternion(30, -30,0,0);
+        lightFlag = false;
 
         if(oldPosX != transform.position.x)
         {
-            posY = this.transform.position.y + 5;
-
-            if (count >= 1.0f)
-            {
-                //コントローラー振動
-                StartVibration(0.5f, 0.5f, 1f);
-                count=0.0f; 
-            }
-            
-            //rb.velocity = new Vector3(Mathf.Clamp((rb.velocity.x * speed),-speed,speed),rb.velocity.y, rb.velocity.z);
-
             //spotLight　ON
             spotLight.SetActive(true);
-            spotLight.transform.position = new Vector3(transform.position.x,posY , spotLight.transform.position.z);
+            spotLight.transform.position = new Vector3(transform.position.x,spotLight.transform.position.y,spotLight.transform.position.z);
 
-            mainLight.transform.rotation = new Quaternion(180, 30, 0, 0);
+            lightFlag = true;
         }
 
-        oldPosX = transform.position.x;
+        oldPosX = this.transform.position.x;
     }
 
-    //コントローラー振動
-    public void Vibration(float l, float r)
+    public  bool GetLightFlag
     {
-        Gamepad.current.SetMotorSpeeds(l, r);
+        get
+        {
+            return lightFlag;
+        }
     }
-
-    public void StopVibration()
-    {
-        Gamepad.current.SetMotorSpeeds(0, 0);
-    }
-    private IEnumerator LoopVibration(float t, float l, float r)
-    {
-        Gamepad.current.SetMotorSpeeds(l, r);
-        yield return new WaitForSeconds(t);
-        //StopVibration();
-    }
-
-    public void StartVibration(float t, float l, float r)
-    {
-        StartCoroutine(LoopVibration(t, l, r));
-        StopVibration();
-    }
-
 }
---
+++ Assets/Script/MainLightActive.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainLightActive : MonoBehaviour
+{
+    [SerializeField]
+    int barreleNum;
+
+    Barrel[] barrele;
+
+    GameObject mainLight;
+
+    GameObject breakWall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        barrele=new Barrel[barreleNum];
+
+        barrele[0] = GameObject.Find("Barrele").GetComponent<Barrel>();
+
+        if (barreleNum != 0)
+        {
+            for (int i=1;i<barreleNum;i++)
+            {
+                barrele[i] = GameObject.Find("Barrele ("+i+")").GetComponent<Barrel>();
+            }
+        }
+
+        mainLight = GameObject.Find("Directional Light").gameObject;
+
+        breakWall = GameObject.Find("Wall_break").gameObject;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        for (int i=0;i<barreleNum;i++)
+        {
+            if (barrele[i].GetLightFlag)
+            {
+                mainLight.SetActive(false);
+            }
+            else
+            {
+                mainLight.SetActive(true);
+            }
+        }
+        
+        if (breakWall==null)
+        {
+            mainLight.SetActive(true);
+        }
+    }
+}
