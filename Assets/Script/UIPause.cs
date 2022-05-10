178916e62fb5cf88507051fa449182f82dccf7ae
松岡育弥 <hmj2122051@stu.o-hara.ac.jp>
2022-05-10 15:01:37 +0900

    ステージクリア、ポーズ中のSE追加、ステージ１０修正

--- Assets/Script/UIPause.cs
+++ Assets/Script/UIPause.cs
@@ -5,10 +5,23 @@ using UnityEngine.InputSystem;
 
 public class UIPause : MonoBehaviour
 {
+    AudioSource audioSource;
+
+    [SerializeField]
+    AudioClip pause;
+
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void OnPause(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Started)
         {
+            audioSource.PlayOneShot(pause);
+
             GetComponent<Select>().Active();
         }
     }
